ac1bae00c0620e3541c60e067e12aa431dd1b55c
jatin <rjjatinrawat9@gmail.com>
2023-11-10 12:53:25 +0530

    vidyo player

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
 import VideoPlayer from "./components/Player/Player";
-import NewPlayer from "./components/NewPlayer";
 
 function App() {
   return (
@@ -9,8 +7,6 @@ function App() {
       <div>
         <VideoPlayer />
       </div>
-
-      {/* <NewPlayer /> */}
     </div>
   );
 }
